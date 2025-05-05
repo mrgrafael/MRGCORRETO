b64803f6daa7b0f67e831a8760c91d853ed8a54c
mrgrafael <mrg.rafaeldietrich@gmail.com>
2025-05-05 12:15:27 -0300

    Add files via upload

---
+++ mrgiaplus_corrigido_final/frontend/src/components/Mascote.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Mascote() {
+  return (
+    <div className="fixed bottom-4 right-4">
+      <img src="/src/assets/moeda.png" alt="Mascote" className="w-16 h-16" />
+    </div>
+  );
+}
