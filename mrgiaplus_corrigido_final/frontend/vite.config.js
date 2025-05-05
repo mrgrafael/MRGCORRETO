b4782549e0c81109d22323c45f4fcf8d552e13cb
mrgrafael <mrg.rafaeldietrich@gmail.com>
2025-05-05 12:33:03 -0300

    vite.config.js

--- mrgiaplus_corrigido_final/frontend/vite.config.js
+++ mrgiaplus_corrigido_final/frontend/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './',
   plugins: [react()],
+  root: '.', // ← GARANTE que o root é a pasta atual
   build: {
     outDir: 'dist',
-    emptyOutDir: true
   }
-});
+})
